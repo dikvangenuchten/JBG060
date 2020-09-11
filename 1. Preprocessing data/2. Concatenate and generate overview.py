14f98573f2a1f73c7b6084f82d15c297fbc419aa
Tom <tom_groot@live.nl>
2020-09-11 08:36:09 +0200

    Resolved some bugs

--- 1. Preprocessing data/2. Concatenate and generate overview.py
+++ 1. Preprocessing data/2. Concatenate and generate overview.py
@@ -29,20 +29,20 @@ def search_and_concat(location):
                 p = re.compile(regex)
                 result = p.search(f"{location}{item}")
 
-                if (result != None):
+                if (result is not None):
                     log_error(f"ERROR: {location}{item}, cannot be parsed into dataframe (1)")
                     continue
 
-                if result == None:
+                if result is None:
                     regex = f"(.*_[a-zA-Z]*)[0-9]*\{file_extension}"
                     p = re.compile(regex)
-                    result = p.search(location + item)
+                    result = p.search(f"{location}{item}")
 
-                    if result == None:
+                    if result is None:
                         # Find Tempxxxxxxx.csv files
                         regex = f"(.*Temp)[0-9]*\{file_extension}"
                         p = re.compile(regex)
-                        result = p.search(location + item)
+                        result = p.search(f"{location}{item}")
                         if result == None:
                             log_error(f"No regex found for file {location}{item}")
                             print(f"No regex found for file {location}{item}")
@@ -61,48 +61,54 @@ def search_and_concat(location):
 
                 # Read as excel if file is excel, otherwise read as table
                 if file_extension == '.xlsx':
-                    table = pd.read_excel(location + item)
+                    table = pd.read_excel(f"{location}{item}")
                 else:
-                    try:
-                        table = pd.read_csv(location + item, delimiter=";", decimal=",")
-                        if table.empty:
-                            table = pd.read_csv(location + item)
-                    except:
-                        log_error(f"ERROR reading: {location}{item}, cannot be parsed into dataframe (2)")
+                    with open(f"{location}{item}", 'r') as file:
+                        data = file.read().replace('\n', '')
+                        p = re.compile("^[^,]*;")
+                        result_del = p.search(data)
 
-                print(f"{location}{item}")
-                try:
-                    table = set_unique_column_to_index(table)
-                    if table is None:
-                        log_error(f"ERROR: table is empty {location}{item}")
-                        continue
-                    if result is not None:
-                        print(f"Found group: {result.group(1)} Item: {item}")
-                        if result.group(1) not in dataframes:
-                            dataframes[result.group(1)] = pd.DataFrame()
-                        old_df = dataframes[result.group(1)]
-                        dataframes[result.group(1)] = pd.concat([old_df, table])
-                        if len(dataframes[result.group(1)]) != len(old_df) + len(table):
-                            log_error(f"ERROR: found duplicate rows in concatenating {location}{item}")
-                    else:
-                        table.to_csv(f"processed/{new_file}.csv")
-                except:
-                    log_error(f"ERROR reading: {location}{item}, index column cannot be set")
+                        if result_del is not None:
+                            table = pd.read_csv(f"{location}{item}", delimiter=";", decimal=",")
+                        else:
+                            table = pd.read_csv(f"{location}{item}")
+
+                if table.columns[0] == 'Column1':
+                    table.columns = table.iloc[9]
+                    table = table[10:].reset_index(drop=True)
+
+                table = set_unique_column_to_index(table)
+                if table is None:
+                    log_error(f"ERROR: table is empty {location}{item}")
+                    continue
+                if result is not None:
+                    print(f"Found group: {result.group(1)} Item: {item}")
+                    if result.group(1) not in dataframes:
+                        dataframes[result.group(1)] = pd.DataFrame()
+                    old_df = dataframes[result.group(1)]
+                    dataframes[result.group(1)] = pd.concat([old_df, table])
+                    if len(dataframes[result.group(1)]) != len(old_df) + len(table):
+                        log_error(f"ERROR: found duplicate rows in concatenating {location}{item}")
+                else:
+                    table.to_csv(f"processed/{new_file}.csv")
 
     save_and_make_overview(dataframes)
 
 
 def save_and_make_overview(dfs):
-    overview = pd.read_csv(location + item)
+    overview = pd.read_excel("processed/1. Overview.xlsx", index_col=0)
     for df_location in dfs:
         new_location = df_location.replace("../", "").replace("/", "_")
         if (dfs[df_location].empty):
             log_error(f"{df_location} is empty...")
             continue
-        overview[new_location] = {}
-        overview[new_location]['columns'] = df_location.columns
+        overview = overview.append({
+            'location': df_location,
+            'columns': ', '.join(dfs[df_location].columns),
+            'nr_rows': len(dfs[df_location])
+        }, ignore_index=True)
         dfs[df_location].to_csv(f"processed/{new_location}.csv")
-    pd.DataFrame.from_dict(overview).to_csv(f"processed/1. Overview.csv")
+    overview.to_excel("processed/1. Overview.xlsx")
 
 
 def set_unique_column_to_index(df):
@@ -119,7 +125,9 @@ def set_unique_column_to_index(df):
 
     index_column = df.nunique().sort_values(ascending=False).index[0]
 
-    if is_date(df.loc[0, index_column]):
+    if (not isinstance(df.loc[0, index_column], int)
+            and not isinstance(df.loc[0, index_column], float)
+            and is_date(df.loc[0, index_column])):
         df[index_column] = pd.to_datetime(df[index_column])
     df.set_index(index_column, inplace=True)
 
