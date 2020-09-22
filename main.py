43f0d22a7581fe5a1da1ad66e7b1df26545fc28f
Tom <tom_groot@live.nl>
2020-09-22 21:01:09 +0200

    Added rough sketch for the model

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from preprocessing import concatenate_and_generate_overview
+from model import lstm
 from preprocessing import unzipify
+import argparse
 
 data_path = "data"
-unzipped = True
 
 if __name__ == '__main__':
-    if not unzipped:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--unzipped', action='store_true', default=False, help='whether we want to do an unzipify')
+    parser.add_argument('-c', '--concat', action='store_true', default=False, help='whether we want to do the concat')
+    parser.add_argument('-m', '--model', action='store_true', default=False, help='whether we want to run the model')
+    args = parser.parse_args()
+
+    if args.unzipped:
         unzipify.search_and_unzip(data_path)
-    print("done unzipify")
-    concatenate_and_generate_overview.search_and_concat(data_path)
+    if args.concat:
+        concatenate_and_generate_overview.search_and_concat(data_path)
+    if args.model:
+        lstm.create_model()
+
+
