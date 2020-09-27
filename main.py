2d022e92f1eb21d527b427581da82afad5d6da06
Dik van Genuchten <dikvangenuchten@gmail.com>
2020-09-27 22:36:48 +0200

    Add working diff prediction model

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from model.diff_predictor import DiffPredictor
 data_path = "data"
 model_dir = "saved_model"
 epochs = 10
+batch_size = 64
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -27,25 +28,24 @@ if __name__ == '__main__':
         concatenate_and_generate_overview.search_and_concat(data_path)
 
     data_handler = DataHandler(data_path)
-    train_data, test_data = data_handler.load_data(batch_size=64)
+    data_handler.load_data(batch_size=64)
 
-    model = DiffPredictor("helftheuvel")
+    model = DiffPredictor("helftheuvel", input_shape=data_handler.x_shape)
     model.build(data_handler.x_shape)
     model.summary()
     model.compile(
         optimizer="rmsprop",
         loss="mse",
         metrics=["mse", "mae"],
-        run_eagerly=True
+        run_eagerly=False
     )
 
     for epoch in range(epochs):
         print(f"Starting Epoch {epoch}")
-        x = np.expand_dims(data_handler[0], [0])
+        train_data = data_handler.train_iterator(batch_size=batch_size)
         model.fit(train_data)
         print(f"Finished training on Epoch {epoch}")
-        print(x)
-        print(model.predict(x))
+        test_data = data_handler.test_iterator(batch_size=batch_size)
         model.evaluate(test_data)
         print(f"Finished evaluation on Epoch {epoch}")
         model.save(os.path.join(model_dir, "checkpoints", str(epoch)))
--- model/diff_predictor.py
+++ model/diff_predictor.py
@@ -4,18 +4,26 @@ from tensorflow.keras.layers import Conv1D, Flatten, Dense
 
 
 class DiffPredictor(tf.keras.Model):
-    def __init__(self, pump_station):
+    def __init__(self, pump_station, input_shape=(None, 48, 3)):
         super().__init__()
         self.conv_1 = Conv1D(4, kernel_size=6, activation="relu", padding="same")
         self.conv_2 = Conv1D(2, kernel_size=6, activation="relu", padding="same")
-        self.conv_3 = Conv1D(1, kernel_size=3, activation="relu", padding="same")
+        self.conv_3 = Conv1D(1, kernel_size=3, activation="linear", padding="same")
+        self.flatten = Flatten()
+        self.out = Dense(48, activation="linear")
+        # super(DiffPredictor, self).__init__(inputs=self.input_layer, output=self.out)
         self.pump_station = pump_station
+        self._input_shape = input_shape
+
+    def get_config(self):
+        return {"pump_station": self.pump_station, "input_shape": self._input_shape}
 
     def call(self, inputs, training=None, mask=None):
         x = self.conv_1(inputs)
         x = self.conv_2(x)
         x = self.conv_3(x)
-        return tf.squeeze(x, axis=-1)
+        x = self.flatten(x)
+        return self.out(x)
 
     def save(self,
              filepath,
