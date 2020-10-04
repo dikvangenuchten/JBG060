f692b2001455f9b54ee40df1169bf2d7f9f867fb
EmmieBessems <49960318+EmmieBessems@users.noreply.github.com>
2020-10-04 16:31:57 +0200

    Updated both files so now they work for hourly data instead of daily data.

--- data_splitting.py
+++ data_splitting.py
@@ -18,8 +18,21 @@ def create_test_data(df, final_days: int):
     '''Takes a dataframe with split date columns and creates test data in the
     following way: for every month, take the last 7 consecutive days.'''
 
-    month_groups = df.groupby(['Year', 'Month'])
-    test_data = month_groups.tail(n=final_days)
+    # Drop the 2017 entry (should be in training)
+    df = df.drop(df[df['Year'] == 2017].index)
+
+    # Filter out the final month, because this month is not complete
+    final_month = df[(df['Year'] == 2020) & (df['Month'] == 8)]
+    final_month_n = ((final_days - 5)) * 24 + 22  # 5 missing days, 1 with 22 hours
+    test_final_month = final_month.tail(final_month_n)
+
+    # Drop the final month and create groups, then take the n last days
+    df_no_final = df.drop(final_month.index)
+    month_groups = df_no_final.groupby(['Year', 'Month'])
+    test_other = month_groups.tail(final_days * 24)
+
+    # Concat final month with the other months
+    test_data = pd.concat([test_other, test_final_month])
 
     return test_data
 
@@ -36,7 +49,7 @@ def create_train_data(df, test_df):
 
 
 if __name__ == '__main__':
-    rainfall_data = pd.read_csv('rainfallpredictions1_1.csv')
+    rainfall_data = pd.read_csv('rainfallpredictionsHourlyV3.csv')
     new_rainfall_data = split_date_column(rainfall_data, 'Time')
     test_data = create_test_data(new_rainfall_data, 7)
     train_data = create_train_data(new_rainfall_data, test_data)
