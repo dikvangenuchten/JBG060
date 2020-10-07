dc17f70719f5990b76d82f250695abc73a19f09b
Bram343 <oscarbroekgaarden@gmail.com>
2020-10-07 19:46:03 +0200

    Added a min capacity, model, flood, pump switch and a pump mode attribute

--- pump_class.py
+++ pump_class.py
@@ -4,14 +4,18 @@ import math
 class Pump:
     '''Class for pumps'''
 
-    def __init__(self, max_capacity, max_pump_flow, start_level, model):
+    def __init__(self, min_capacity, max_capacity, max_pump_flow, start_level, model):
         '''A pump needs to have a max capacity, max pump flow and a starting level and a model'''
 
+        self.min_capacity = min_capacity
         self.max_capacity = max_capacity
         self.max_pump_flow = max_pump_flow
         self.level = start_level
         self.model = model
         self.predicted_level = start_level
+        self.flood = 0
+        self.pump_changes = 0
+        self.pump_mode = 'Off'
 
     def __str__(self):
         """prints the level of a model and the predicted level"""
@@ -26,13 +30,22 @@ class Pump:
 
         if self.level > self.max_capacity:
             self.level = self.max_capacity
-            print('Overflow!')  # Probably needs to be changed to something else
-        if self.level < 0:
-            self.level = 0
+            self.flood += 1
+        if self.level < self.min_capacity:
+            self.level = self.min_capacity
 
-    def predict_level(self, pump_level, predicted_incoming_water):
+        if self.pump_mode == "Off" and pump_level > 0:
+            self.pump_mode = "On"
+            self.pump_changes += 1
+
+        if self.pump_mode == "On" and pump_level == 0:
+            self.pump_mode = "Off"
+            self.pump_changes += 1
+
+    def predict_level(self, pump_level):
         """predicts the level for the next hours"""
 
+        predicted_incoming_water = self.model()
         self.predicted_level = self.level + predicted_incoming_water - pump_level * self.max_pump_flow
 
         if self.predicted_level > self.max_capacity:
@@ -52,6 +65,6 @@ class Pump:
 
         pumped = pump_level * self.max_pump_flow
 
-        if pumped > self.level:
-            pumped = self.level
+        if pumped > (self.level-self.min_capacity):
+            pumped = self.level - self.min_capacity
         return pumped
